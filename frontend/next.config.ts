95e79cafe989dced63e55cf0c454bb69c268a919
padhipradyumna90 <padhipradyumna90@gmail.com>
2025-12-31 18:50:06 +0530

    disable eslint during build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['bookbins.b-cdn.net'], // Add your image host here
+//   },
+// };
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['bookbins.b-cdn.net'], // Add your image host here
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
+
+module.exports = nextConfig;
+
 
 module.exports = nextConfig;
